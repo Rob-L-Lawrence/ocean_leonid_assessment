76f731f9a8b6aff604dc43d6b6f0f75e398b8a61
Rob <rob.luke.lawrence@gmail.com>
2023-08-25 11:44:49 +0100

    layout complete

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # README
 
 This is a project written by Robert Lawrence for the purposes of the Ocean Leonid assessment.
+
+## Distribution
+
+**Github**
+
+To get started with the raw code, clone the repo and then run the following commands:
+
+```shell
+pip install poetry
+poetry install
+python ./ocean_leonid/run.py
+```
+
+The dash app is now available on your browser at `http://localhost:8050`.
+
+**Docker**
+
+To deploy the dash app using docker use the following commands:
+
+```shell
+docker build . -t ocean_leonid
+docker container run -p 8050:8050  ocean_leonid
+```
+
+The dash app is now available on your browser at `http://localhost:8050`.
+
+## Tests
+
+## External Dependencies
+
+The external dependencies of this solution are the 2 API's that are called to get the data for the graph.
+
+## Design Decisions
--- ocean_leonid/src/layout.py
+++ ocean_leonid/src/layout.py
@@ -1,5 +1,40 @@
-from dash import html
+import plotly.graph_objs as go
+from dash import dcc, html
+
+from .apis import get_combine_data
 
 
 def layout():
-    return html.Div([html.Div(children="Hello World")])
+    df = get_combine_data()
+
+    fig = go.Figure()
+
+    uk_values = go.Scatter(
+        x=df["date"],
+        y=df["annualised_rate_uk"],
+        name="UK CPIH",
+        mode="lines",
+    )
+    fig.add_trace(uk_values)
+
+    au_values = go.Scatter(
+        x=df["date"],
+        y=df["annualised_rate_au"],
+        name="AU CPI",
+        mode="lines",
+    )
+    fig.add_trace(au_values)
+
+    fig.update_layout(
+        xaxis_title="Date",
+        yaxis_title="%",
+        title="Inflation (CPI)",
+        showlegend=True,
+        legend={"title": "Variable"},
+    )
+
+    return html.Div(
+        [
+            dcc.Graph(id="inflation_graph", figure=fig),
+        ],
+    )
